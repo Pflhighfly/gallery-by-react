4204b1c88662375c07451d82ab0fc74f68937850
pfl <2211869388@qq.com>
2016-07-15 12:28:19 +0800

    test3 is ok html can see

--- src/components/GalleryByReactApp.js
+++ src/components/GalleryByReactApp.js
@@ -20,10 +20,21 @@ imageDatas = (function genImageURL(imageDatasArr) {
      return imageDatasArr;
 })(imageDatas);
 // imageDatas =genImageURL(imageDatas);
+
+function getRangeRandom(low, high){
+  return Math.ceil(Math.random() * (high - low) + low);
+}
+
 var ImgFigure = React.createClass({
   render: function() {
+   var styleObj = {};
+     // 如果props属性中指定了这张图片的位置则使用
+  if(this.props.arrange.pos){
+     styleObj = this.props.arrange.pos;
+  }
+
      return (
-       <figure className = "img-figure">
+       <figure className = "img-figure" style = {styleObj}>
        <img src = {this.props.data.imageURL}
             alt = {this.props.data.title}
           />
@@ -50,29 +61,112 @@ var GalleryByReactApp = React.createClass({
        topY: [0, 0]
      }
    },
+
+   rearrange: function(centerIndex){
+    var imgsArrangeArr = this.state.imgsArrangeArr,
+       Constant = this.Constant,
+       centerPos = Constant.centerPos,
+       hPosRange = Constant.hPosRange,
+       vPosRange = Constant.vPosRange,
+       hPosRangeLeftSecx = hPosRange.leftSecx,
+       hPosRangeRightSecx = hPosRange.rightSecx,
+       hPosRangeY = hPosRange.y,
+       vPosRangeTopY = vPosRange.topY,
+       vPosRangeX = vPosRange.x,
+
+       imgsArrangeTopArr = [],
+       topImgNum = Math.ceil(Math.random() * 2),//取一个或或者不去
+       topImgSpliceIndex = 0,
+       imgsArrangeCenterArr = imgsArrangeArr.splice(centerIndex, 1);
+      // 首先剧中 centerIndex 的图片
+      imgsArrangeCenterArr[0].pos = centerPos;
+
+      // 取出要布局上侧的图片状态信息
+      topImgSpliceIndex = Math.ceil(Math.random() * (imgsArrangeArr.length - topImgNum));
+      imgsArrangeTopArr = imgsArrangeArr.splice(topImgSpliceIndex, topImgNum);
+
+      //布局位于上侧的图片
+      imgsArrangeTopArr.forEach(
+        function(value, index){
+          imgsArrangeTopArr[index].pos = {
+            top: getRangeRandom(vPosRangeTopY[0], vPosRangeTopY[1]),
+            left: getRangeRandom(vPosRangeX[0], vPosRangeX[1])
+          };
+        }
+      );
+      //布局左右两层的图片信息
+      for(var i = 0, j = imgsArrangeArr.length, k = j / 2; i < j; i++){
+        var hPosRangeLORX = null;
+        // 前半部分布局左边，后半部分布局右边
+        if(i < k){
+          hPosRangeLORX = hPosRangeLeftSecx;
+        }else{
+          hPosRangeLORX = hPosRangeRightSecx;
+        }
+
+        imgsArrangeArr[i].pos = {
+          top: getRangeRandom(hPosRangeY[0], hPosRangeY[1]),
+          left: getRangeRandom(hPosRangeLORX[0], hPosRangeLORX[1])
+        };
+      }
+             if(imgsArrangeTopArr && imgsArrangeTopArr[0]){
+               imgsArrangeArr.splice(topImgSpliceIndex, 0, imgsArrangeTopArr[0]);
+             }
+
+             imgsArrangeArr.splice(centerIndex, 0, imgsArrangeCenterArr[0]);
+             this.setState({
+                  imgsArrangeArr: imgsArrangeArr
+             });
+   },
+
+getInitialState: function(){
+  return {
+    imgsArrangeArr: []
+  };
+},
+
    //组件加载以后，为每张图片计算其位置
 componentDidMount: function (){
    var stageDOM = React.findDOMNode(this.refs._stage),
    stageW = stageDOM.scrollWidth,
    stageH = stageDOM.scrollHeight,
-   halfStageW = Math.cell(stageW / 2),
-   halfStageH = Math.cell(stageH / 2);
+   halfStageW = Math.ceil(stageW / 2),
+   halfStageH = Math.ceil(stageH / 2);
    var imgFigureDOM = React.findDOMNode(this.refs.ImgFigure0),
    imgW = imgFigureDOM.scrollWidth,
    imgH = imgFigureDOM.scrollHeight,
-   halfImgW = Math.cell(imgW / 2),
-   halfImgH = Math.cell(imgH / 2);
-   this.Constant.centerPos={
-     left: halfStageW-halfImgW,
-     top: halfStageH-halfImgH
-   }
+   halfImgW = Math.ceil(imgW / 2),
+   halfImgH = Math.ceil(imgH / 2);
+   this.Constant.centerPos = {
+     left: halfStageW - halfImgW,
+     top: halfStageH - halfImgH
+   };
+  this.Constant.hPosRange.leftSecx[0] = -halfImgW;
+  this.Constant.hPosRange.leftSecx[1] = halfStageW - halfImgW * 3;
+  this.Constant.hPosRange.rightSecx[0] = halfStageW + halfImgW;
+  this.Constant.hPosRange.rightSecx[1] = stageW - halfImgW;
+  this.Constant.hPosRange.y[0] = -halfImgH;
+  this.Constant.hPosRange.y[1] = stageH - halfImgH;
+  this.Constant.vPosRange.topY[0] = -halfImgH;
+  this.Constant.vPosRange.topY[1] = halfStageH - halfImgH * 3;
+  this.Constant.vPosRange.x[0] = halfStageW - imgW;
+  this.Constant.vPosRange.x[1] = halfStageW;
+   this.rearrange(0);
  },
    render: function() {
     //  var controllerUnits = [];
      var ImgFigures = [];
-        imageDatas.forEach(function(value){
-          ImgFigures.push(<ImgFigure data={value} ref={'ImgFigure' + index}/>);
-        });
+        imageDatas.forEach(function(value, index){
+          if(!this.state.imgsArrangeArr[index]){
+            this.state.imgsArrangeArr[index] = {
+              pos: {
+                left: 0,
+                top: 0
+              }
+            };
+          }
+          ImgFigures.push(<ImgFigure data={value} ref={'ImgFigure' + index} arrange={this.state.imgsArrangeArr[index]}/>);
+        }.bind(this));
     return (
       <section className = "stage" ref = "_stage">
               <section className = "img-sec">
